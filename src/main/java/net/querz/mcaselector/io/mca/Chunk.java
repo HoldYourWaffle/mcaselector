56d536d09c483ea69fb58cdde650e1cb5df875dc
HoldYourWaffle <ravivanrooijen@live.nl>
2022-07-14 16:53:37 +0200

    De-dupe Chunk implementations' relocate, merge and getMCCFile

--- src/main/java/net/querz/mcaselector/io/mca/Chunk.java
+++ src/main/java/net/querz/mcaselector/io/mca/Chunk.java
@@ -1,20 +1,22 @@
 package net.querz.mcaselector.io.mca;
 
 import net.querz.mcaselector.io.ByteArrayPointer;
+import net.querz.mcaselector.io.FileHelper;
 import net.querz.mcaselector.point.Point2i;
 import net.querz.mcaselector.point.Point3i;
 import net.querz.mcaselector.range.Range;
 import net.querz.mcaselector.validation.ValidationHelper;
+import net.querz.mcaselector.version.VersionController;
+import net.querz.nbt.CompoundTag;
 import net.querz.nbt.NBTUtil;
 import net.querz.nbt.Tag;
 import net.querz.nbt.io.NBTReader;
 import net.querz.nbt.io.NBTWriter;
-import net.querz.nbt.CompoundTag;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -46,9 +48,9 @@ public abstract class Chunk implements Cloneable {
 			case GZIP -> new DataInputStream(new BufferedInputStream(new GZIPInputStream(ptr, length)));
 			case ZLIB -> new DataInputStream(new BufferedInputStream(new InflaterInputStream(ptr, new Inflater(), length)));
 			case NONE -> new DataInputStream(ptr);
-			case GZIP_EXT -> new DataInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(getMCCFile()))));
-			case ZLIB_EXT -> new DataInputStream(new BufferedInputStream(new InflaterInputStream(new FileInputStream(getMCCFile()))));
-			case NONE_EXT -> new DataInputStream(new BufferedInputStream(new FileInputStream(getMCCFile())));
+			case GZIP_EXT -> new DataInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation)))));
+			case ZLIB_EXT -> new DataInputStream(new BufferedInputStream(new InflaterInputStream(new FileInputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation)))));
+			case NONE_EXT -> new DataInputStream(new BufferedInputStream(new FileInputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation))));
 		};
 
 		Tag tag = new NBTReader().read(nbtIn);
@@ -68,9 +70,9 @@ public abstract class Chunk implements Cloneable {
 			case GZIP -> new DataInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(raf.getFD()))));
 			case ZLIB -> new DataInputStream(new BufferedInputStream(new InflaterInputStream(new FileInputStream(raf.getFD()))));
 			case NONE -> new DataInputStream(new BufferedInputStream(new FileInputStream(raf.getFD()), length - 1));
-			case GZIP_EXT -> new DataInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(getMCCFile()))));
-			case ZLIB_EXT -> new DataInputStream(new BufferedInputStream(new InflaterInputStream(new FileInputStream(getMCCFile()))));
-			case NONE_EXT -> new DataInputStream(new BufferedInputStream(new FileInputStream(getMCCFile())));
+			case GZIP_EXT -> new DataInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation)))));
+			case ZLIB_EXT -> new DataInputStream(new BufferedInputStream(new InflaterInputStream(new FileInputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation)))));
+			case NONE_EXT -> new DataInputStream(new BufferedInputStream(new FileInputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation))));
 		};
 
 		Tag tag = new NBTReader().read(nbtIn);
@@ -107,7 +109,7 @@ public abstract class Chunk implements Cloneable {
 
 			raf.writeInt(1);
 			raf.writeByte(compressionType.getExternal().getByte());
-			try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(getMCCFile()), baos.size())) {
+			try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(FileHelper.createMCCFilePath(getType(), absoluteLocation)), baos.size())) {
 				bos.write(baos.getBuffer(), 0, baos.size());
 			}
 			return 5;
@@ -119,11 +121,13 @@ public abstract class Chunk implements Cloneable {
 		}
 	}
 
-	public abstract boolean relocate(Point3i offset);
+	public boolean relocate(Point3i offset) {
+		return VersionController.getChunkRelocator(getType(), getDataVersion()).relocate(data, offset);
+	}
 
-	public abstract void merge(CompoundTag destination, List<Range> ranges, int yOffset);
-
-	public abstract File getMCCFile();
+	public void merge(CompoundTag destination, List<Range> ranges, int yOffset) {
+		VersionController.getChunkMerger(getType(), getDataVersion()).mergeChunks(data, destination, ranges, yOffset);
+	}
 
 	public boolean isEmpty() {
 		return data == null;
