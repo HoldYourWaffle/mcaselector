8c298f7ab1491959e55a6cb544f5418765535ade
HoldYourWaffle <ravivanrooijen@live.nl>
2022-07-14 16:53:37 +0200

    De-dupe Chunk implementations' relocate, merge and getMCCFile

--- src/main/java/net/querz/mcaselector/io/mca/PoiChunk.java
+++ src/main/java/net/querz/mcaselector/io/mca/PoiChunk.java
@@ -1,37 +1,11 @@
 package net.querz.mcaselector.io.mca;
 
-import net.querz.mcaselector.io.FileHelper;
 import net.querz.mcaselector.point.Point2i;
-import net.querz.mcaselector.point.Point3i;
-import net.querz.mcaselector.range.Range;
-import net.querz.mcaselector.version.ChunkMerger;
-import net.querz.mcaselector.version.ChunkRelocator;
-import net.querz.mcaselector.version.VersionController;
-import net.querz.nbt.CompoundTag;
-import java.io.File;
-import java.util.List;
 
 public class PoiChunk extends Chunk {
 
 	public PoiChunk(Point2i absoluteLocation) {
 		super(absoluteLocation);
-	}
-
-	@Override
-	public boolean relocate(Point3i offset) {
-		ChunkRelocator relocator = VersionController.getChunkRelocator(McaType.POI, data.getInt("DataVersion"));
-		return relocator.relocate(data, offset);
-	}
-
-	@Override
-	public void merge(CompoundTag destination, List<Range> ranges, int yOffset) {
-		ChunkMerger merger = VersionController.getChunkMerger(McaType.POI, data.getInt("DataVersion"));
-		merger.mergeChunks(data, destination, ranges, yOffset);
-	}
-
-	@Override
-	public File getMCCFile() {
-		return FileHelper.createMCCFilePath(McaType.POI, absoluteLocation);
 	}
 
 	@Override
